db1490b9deea83efd09c64bccb3eb8cfb9f07525
gabdlr <gab.delosrios@gmail.com>
2021-08-16 17:42:00 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio1/App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Ejercicio1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+               name: "Usuario",
+               url: "{controller}/{action}/{nombre}",
+               defaults: new { controller = "Usuario", action = "Index", nombre = UrlParameter.Optional }
+           );
+
+            
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+
+            
+             
+        }
+    }
+}
---
+++ Ejercicio1/Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejercicio1.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+
+        public ActionResult Buscar(string nombre)
+        {
+            var input = Server.HtmlEncode(nombre);
+            input = "<h1>" + input + "</h1>" + 
+        "<input action = \"action\" onclick = \"window.history.go(-1); return false;\" type = \"submit\" value = \"Volver\"/> ";
+            //Se que esto no es lo mas adecuado pero por cuestiones de tiempo decidi dejarlo de esta manera.
+        return Content(input);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+
+
+}
